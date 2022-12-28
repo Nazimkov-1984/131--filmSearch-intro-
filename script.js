185703016a868d4025a90405b1be11831d500adc
Mykola Nazimkov <nazimkoff@gmail.com>
2022-12-28 12:01:09 +0200

    fix bug

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const inputFilm = document.querySelector('input')
 const button = document.querySelector('button')
 const card = document.querySelector('.card')
 
+setInterval(function() {
+    if(inputFilm.value !== '') {
+        button.disabled = false
+    } else {
+        button.disabled = true
+    }
+}, 500)
+
 // background-color - зміна стилей через JavaScript
 // button.style.backgroundColor = 'red'
 // inputFilm.style.backgroundColor = 'red'
@@ -23,50 +31,93 @@ const card = document.querySelector('.card')
 // button.addEventListener('mouseenter', functionClick)
 // button.addEventListener('mouseleave', functionLeave)
 
-function printFilmName() {
+function clearResults () {
+const searchResults = document.querySelector('.searchResults')
+const cards = document.querySelectorAll('.card')
+if(cards) {
+    cards.forEach(function(card) {
+        searchResults.removeChild(card)
+    })
+}
+}
+
+function createCard (src,title, year, type) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.style.width = '18rem'
+    const image = document.createElement('img')
+    image.classList.add("card-img-top")
+    const cardBody = document.createElement('div')
+    cardBody.classList.add("card-body")
+    const cardTitle = document.createElement('h3')
+    cardTitle.classList.add("card-title")
+    const cardText = document.createElement('h3')
+    cardText.classList.add("card-text")
+    const typeFilmWrapper = document.createElement('div')
+    typeFilmWrapper.classList.add('type-film')
+    const typeText = document.createElement('p')
+    typeText.classList.add('card-text')
+    typeText.textContent = 'Type'
+    const filmType = document.createElement('p')
+    filmType.classList.add('card-text')
+    filmType.classList.add('type')
+    const button = document.createElement('a')
+    button.classList.add('btn')
+    button.classList.add('btn-primary')
+    button.textContent = 'Learn more'
+    // додаэмо елементи в правильному порядку
+    card.appendChild(image)
+    card.appendChild(cardBody)
+    cardBody.appendChild(cardTitle)
+    cardBody.appendChild(cardText)
+    cardBody.appendChild(typeFilmWrapper)
+    cardBody.appendChild(button)
+    typeFilmWrapper.appendChild(typeText)
+    typeFilmWrapper.appendChild(filmType)
+    const searchResults = document.querySelector('.searchResults')
+    searchResults.appendChild(card)
+    // наповнення картки контентом
+    image.src = src
+    cardTitle.textContent = title
+    cardText.textContent = year
+    filmType.textContent = type
+}
+
+function onButtonClick () {
+    clearResults()
     const film = inputFilm.value
     fetch(`http://www.omdbapi.com/?r=json&s=${film}&apikey=2a7d7e9f&`)
         .then(response => response.json())
         .then(json => {
-            json.Search.forEach(function (film) {
-                // Poster: "https://m.media-amazon.com/images/M/MV5BM2M1MmVhNDgtNmI0YS00ZDNmLTkyNjctNTJiYTQ2N2NmYzc2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-                // Title: "Taxi Driver"
-                // Type: "movie"
-                // Year: "1976"
-                // imdbID: "tt0075314"
-                console.log(film.Title)
-                const poster = document.querySelector('.card-img-top')
-                const title = document.querySelector('.card-title')
-                const year = document.querySelector(".card-text")
-                const type = document.querySelector('.type')
-                poster.src = film.Poster
-                poster.alt = film.Title
-                title.textContent = film.Title
-                year.textContent = film.Year
-                type.textContent = film.Type
+            if(json.Search) {
+                json.Search.forEach(function (film) {
+                createCard(film.Poster, film.Title, film.Year, film.Type)
             })
+            } else {
+                alert('Movie not found!')
+            }
         })
 }
 
-button.addEventListener('click', printFilmName)
+button.addEventListener('click', onButtonClick)
 
 // два типу данних - це об'єкти та масиви (number, string, boolean - /true, false)
 // array та object
 //МАСИВИ 
 
-const students131 = "Даніл Ілля Аня Сергій Вова Богдан"
+// const students131 = "Даніл Ілля Аня Сергій Вова Богдан"
 
-const studentsNames = ["Даніл", "Ілля", "Аня", "Сергій", 'Вова', "Богдан"]
+// const studentsNames = ["Даніл", "Ілля", "Аня", "Сергій", 'Вова', "Богдан"]
 
 // кожен елемент масиву має свій індекс елементу який починаєтся з нуля
 
-let student = ['Даніл', 16, 4, true, false, undefined, ['html', 'css', 'js']]
+// let student = ['Даніл', 16, 4, true, false, undefined, ['html', 'css', 'js']]
 
 // перебор массиву forEach()
 
-studentsNames.forEach(function (element, ) {
+// studentsNames.forEach(function (element, ) {
     // console.log(element)
-})
+// })
 // властивість массиву - це те що ми можемо отримати його довжину (length)
 
 // console.log('Всього в групі - ', studentsNames.length, "студентів.")
@@ -74,14 +125,44 @@ studentsNames.forEach(function (element, ) {
 // об'єкти - на відміну від масивів кожний елемент в об'єті має своє ім'я
 // всі данні в об'єкті зберігаются в вигляді пари "ключ":"значення"
 
-const oneStudent = {
-    name: "Даніл",
-    age: 16,
-    months: 4,
-    isHomeworkDone: true, //чиВиконаніДомашки?
-    isFinalProjectDone: false, // чиВиконанийФінальнийПроект?
-    petName: undefined,
-    technologies: ['html', 'css', 'js']
-}
+// const oneStudent = {
+//     name: "Даніл",
+//     age: 16,
+//     months: 4,
+//     isHomeworkDone: true, //чиВиконаніДомашки?
+//     isFinalProjectDone: false, // чиВиконанийФінальнийПроект?
+//     petName: undefined,
+//     technologies: ['html', 'css', 'js']
+// }
 
-// console.log(oneStudent.name)
+// створення елементів за допомогою JS 
+
+// document.createElement - функція для створення елементів
+// const newElement = document.createElement('div')
+// const newImage = document.createElement('img')
+
+// appendChild - функція для додавання елементів в різні контейнери 
+// container.appendChild(newElement)- умовна структура для додавання нового елемента
+
+// const searchResults =  document.querySelector('.searchResults')
+
+// searchResults.appendChild(newElement)
+// searchResults.appendChild(newImage)
+
+//  <!-- <div class="card" style="width: 18rem;">
+//         <img src="" class="card-img-top" alt="">
+//         <div class="card-body">
+//             <h3 class="card-title"></h3>
+//             <p class="card-text"></p>
+//             <div class="type-film">
+//                 <p class="card-text">Type:  </p>
+//                 <p class="card-text type"></p> 
+//             </div>
+//             <a href="#" class="btn btn-primary">Learn more</a>
+//         </div>
+//     </div> -->
+
+// classList.toggle('my-class')  - якщо в елемента нема такого классу: він додається, 
+// якщо в елемента є такий класс: то він видаляється
+
+
